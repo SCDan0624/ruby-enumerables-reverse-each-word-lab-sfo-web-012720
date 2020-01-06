6b60f888a495b0650d33c21fa55d5e180403c645
Daniel Mayle <dmayle012@gmail.com>
2020-01-06 21:25:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,4 @@
 def reverse_each_word(string)
-  
+  string.split.map {|string| string.reverse}.join(" ")
 end
 
-# def reverse_each_word(sentence)
-#   sentence.split.collect {|word| word.reverse}.join(" ")
-# end
